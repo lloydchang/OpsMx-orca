32a3950d442f8883c6d7eb114a3f5687c1a6605a
Yugandharkumar <yugaa22@gmail.com>
2023-03-27 19:10:04 +0530

    temp fix for kork dir
    optional:${user.home}/.spinnaker/
    on recomendation of luthan

--- orca-web/src/main/groovy/com/netflix/spinnaker/orca/Main.groovy
+++ orca-web/src/main/groovy/com/netflix/spinnaker/orca/Main.groovy
@@ -75,7 +75,7 @@ class Main extends SpringBootServletInitializer {
     'netflix.environment'              : 'test',
     'netflix.account'                  : '${netflix.environment}',
     'netflix.stack'                    : 'test',
-    'spring.config.additional-location': '${user.home}/.spinnaker/',
+    'spring.config.additional-location': 'optional:${user.home}/.spinnaker/',
     'spring.application.name'          : 'orca',
     'spring.config.name'               : 'spinnaker,${spring.application.name}',
     'spring.profiles.active'           : '${netflix.environment},local'
